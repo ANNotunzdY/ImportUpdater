60e49bda440a382bee3d99a3ce1425bca20fbbcd
Takayoshi Sato <webmaster@smilemac.com>
2016-06-28 23:49:08 +0900

    Initial Commit

---
+++ ImportUpdater/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImportUpdater
+//
+//  Created by ANNotunzdY on 6/28/16.
+//  Copyright © 2016 Kibousoft LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
